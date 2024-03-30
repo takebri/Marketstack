5c8b0a8fce83ddf2cd67b737b42dff38703a3713
Brian Takeda <takebri@gmail.com>
2024-03-29 19:27:47 -0700

    added configure import, api_key, symbol, url

--- main.py
+++ main.py
@@ -2,3 +2,11 @@
 
 import requests
 import csv
+from configure import Marketstack_API_KEY
+
+# Marketstack API key
+api_key = Marketstack_API_KEY
+# The stock symbol
+symbol = 'ORCL'
+# The Marketstack endpoint for end-of-day stock data
+url = f'http://api.marketstack.com/v1/eod?access_key={api_key}&symbols={symbol}'
