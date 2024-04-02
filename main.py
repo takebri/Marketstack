acb59feb400b0eaf1e8f38086a05b88a269bbf0a
Brian Takeda <takebri@gmail.com>
2024-04-01 20:56:33 -0700

    Added write and save api response to csv

--- main.py
+++ main.py
@@ -9,10 +9,31 @@ api_key = Marketstack_API_KEY
 # The stock symbol
 symbol = 'ORCL'
 # The Marketstack endpoint for end-of-day stock data
-url = f'http://api.marketstack.com/v1/eod?access_key={api_key}&symbols={symbol}'
+url = f'http://api.marketstack.com/v1/eod?access_key={api_key}& symbols={symbol}'
+# CSV output
+output_csv = 'stock_data.csv'
+
 
 # Make the API request
 response = requests.get(url)
 
 if response.status_code == 200:
-    data = response.json()
+    stock_data = response.json()
+
+    # stock_data['data'] contains the returned data object
+    with open(output_csv, 'w', newline='') as file:
+        writer = csv.writer(file)
+        # Write the header
+        writer.writerow(['Date', 'Open', 'High', 'Low', 'Close', 'Volume'])
+
+        # Write the stock data
+        for item in stock_data['data']:
+            writer.writerow([
+                item['date'],
+                item['open'],
+                item['high'],
+                item['low'],
+                item['close'],
+                item['volume']]
+                )
+    print(f'Stock data successfully written to {output_csv}')
