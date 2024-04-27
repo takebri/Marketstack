885c7b8133d5bae72dcd6dfad7a160c1fc2b1adb
Brian Takeda <takebri@gmail.com>
2024-04-26 17:07:00 -0700

    add main func

--- api_to_bigquery.py
+++ api_to_bigquery.py
@@ -58,68 +58,18 @@ def insert_into_bigquery(client, table_id, rows_to_insert):
     return
 
 
-# Initialize BigQuery client
-client = bigquery.Client()
-
-# Google Cloud project ID, dataset, table name
-table_id = f"{project_id}.{dataset_name}.{table_name}"
-
-# Parameters
-params = {
-    'access_key': api_key,
-    'symbols': 'ORCL'
-}
-
-# API endpoint for stock data
-url = 'http://api.marketstack.com/v1/eod'
-
-try:
-    # API request
-    response = requests.get(url, params=params)
-    # Raise exception for HTTP error
-    response.raise_for_status()
-
-    # check for successful request
-    if response.ok:
-        stock_data = response.json()['data']
-
-        # Check for data
+def main():
+    try:
+        client = bigquery.Client()
+        table_id = f"{project_id}.{dataset_name}.{table_name}"
+        stock_data = fetch_stock_data(api_key, 'ORCL')
         if stock_data:
-            # save fetched data
-            rows_to_insert = [
-                {
-                    'Date': datetime.fromisoformat(row['date']).date()
-                                    .strftime('%Y-%m-%d'),
-                    'Open': row['open'],
-                    'High': row['high'],
-                    'Low': row['low'],
-                    'Close': row['close'],
-                    'Volume': row['volume']
-                }
-                for row in stock_data
-            ]
+            transformed_data = transform_stock_data(stock_data)
+            insert_into_bigquery(client, table_id, transformed_data)
+    except Exception as e:
+        logging.error(f"An unexpected error occurred: {e}")
+    return
 
-        # Insert data into bigquery
-        errors = client.insert_rows_json(table_id, rows_to_insert)
-        # No error inserting into BigQuery
-        if not errors:
-            logging.info(f"Added {len(rows_to_insert)} rows into {table_id}.")
-        # Error inserting into BigQuery
-        else:
-            logging.error(f"Error during BigQuery insertion: {errors}")
-    # Error retrieving data
-    else:
-        print('Failed to retrieve data. HTTP Status Code:',
-              response.status_code)
 
-# API errors
-except requests.RequestException as e:
-    logging.error(f"Error during API request: {e}")
-
-# BigQuery Error
-except GoogleAPIError as e:
-    logging.error(f"Error inserting data into BigQuery: {e}")
-
-# general errors
-except Exception as e:
-    logging.error(f"An unexpected error occurred: {e}")
+if __name__ == "__main__":
+    main()
